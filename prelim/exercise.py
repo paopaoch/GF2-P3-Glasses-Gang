65c5848c464ab5b3923215776402091b283ae47a
ki277 <ki277@cam.ac.uk>
2023-05-12 12:55:30 +0100

    prelim task 7 completed

--- prelim/exercise.py
+++ prelim/exercise.py
@@ -101,64 +101,59 @@ def main():
     # Check command line arguments
     arguments = sys.argv[1:]
     path = sys.argv[1]
-    print(path)
-    fo = open_file(path)
-
-    # Iterate over the characters in the file
-    while True:
-        char = get_next_character(fo)
-        if not char:
-            break
-        print(char, end='')
-
-    # Reset the file pointer to the beginning of the file
-    fo.seek(0)
-
-    # Iterate over the non-whitespace characters in the file
-    while True:
-        char = get_next_non_whitespace_character(fo)
-        if not char:
-            break
-        print(char, end='')
-
-    # Reset the file pointer to the beginning of the file
-    fo.seek(0)
-
-    # Iterate over the numbers in the file
-    while True:
-        number = get_next_number(fo)
-        if not number:
-            break
-        print(number[0], end=' ')  # print the integer representation of the number
-        if number[1] == '':
-            break
-
-    fo.seek(0)  # reset file pointer to beginning of file
-    name, char = get_next_name(fo)
-    while name is not None:
-        print(name)
-        name, char = get_next_name(fo)
 
     if len(arguments) != 1:
         print("Error! One command line argument is required.")
         sys.exit()
 
-    else:
-
+    else: 
         print("\nNow opening file...")
         # Print the path provided and try to open the file for reading
+        print(path)
+        fo = open_file(path)
 
         print("\nNow reading file...")
         # Print out all the characters in the file, until the end of file
+        # Iterate over the characters in the file
+        while True:
+            char = get_next_character(fo)
+            if not char:
+                break
+            print(char, end='')
 
         print("\nNow skipping spaces...")
         # Print out all the characters in the file, without spaces
+        # Reset the file pointer to the beginning of the file
+        fo.seek(0)
+
+        # Iterate over the non-whitespace characters in the file
+        while True:
+            char = get_next_non_whitespace_character(fo)
+            if not char:
+                break
+            print(char, end='')
 
         print("\nNow reading numbers...")
         # Print out all the numbers in the file
+        # Reset the file pointer to the beginning of the file
+        fo.seek(0)
+
+        # Iterate over the numbers in the file
+        while True:
+            number = get_next_number(fo)
+            if not number:
+                break
+            print(number[0], end=' ')  # print the integer representation of the number
+            if number[1] == '':
+                break
 
         print("\nNow reading names...")
         # Print out all the names in the file
+        fo.seek(0)  # reset file pointer to beginning of file
+        name, char = get_next_name(fo)
+        while name is not None:
+            print(name)
+            name, char = get_next_name(fo)
 
         print("\nNow censoring bad names...")
         # Print out only the good names in the file
