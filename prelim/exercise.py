9647c79dbe6527c2b840aaace15df3deb21325b2
ki277 <ki277@cam.ac.uk>
2023-05-12 12:07:21 +0100

    add prelim files

---
+++ prelim/exercise.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+"""Preliminary exercises for Part IIA Project GF2."""
+import sys
+import os
+from mynames import MyNames
+
+
+def open_file(path):
+    """Open and return the file specified by path."""
+    try:
+        f = open(path, "r")
+        return f
+    except IOError:
+        print ("Error: can\'t find file or read data")
+
+
+def get_next_character(input_file):
+    """Read and return the next character in input_file."""
+    try:
+        character = input_file.read(1)
+        if character:
+            return character
+        else:
+            return ""
+    except IOError:
+        print ("Error: can\'t find file or read data")
+
+
+
+def get_next_non_whitespace_character(input_file):
+    """Seek and return the next non-whitespace character in input_file."""
+    try:
+        while True:
+            character = input_file.read(1)
+            if not character:
+                return ""
+            elif not character.isspace():
+                return character
+    except IOError:
+        print ("Error: can\'t find file or read data")        
+    
+
+
+def get_next_number(input_file):
+    """Seek the next number in input_file.
+
+    Return the number (or None) and the next non-numeric character.
+    """
+    try:
+        number = ''
+        while True:
+            character = input_file.read(1)
+            if not character:
+                if not number:
+                    return None
+                else:
+                    return [int(number), '']
+            elif character.isdigit():
+                number += character
+            else:
+                if number:
+                    return [int(number), character]
+                
+        return None
+    except IOError:
+        print ("Error: can\'t find file or read data") 
+
+
+def get_next_name(input_file):
+    """Seek the next name string in input_file.
+
+    Return the name string (or None) and the next non-alphanumeric character.
+    """
+    try:
+        name = ""
+        char = input_file.read(1)
+        while char and not char.isalpha():
+            char = input_file.read(1)
+        if not char:
+            return [None, ""]
+
+        name += char
+        char = input_file.read(1)
+        while char and char.isalnum():
+            name += char
+            char = input_file.read(1)
+
+        if not char:
+            return [name, ""]
+        else:
+            return [name, char]
+        
+    except IOError:
+        print ("Error: can\'t find file or read data") 
+
+
+
+def main():
+    """Preliminary exercises for Part IIA Project GF2."""
+
+    # Check command line arguments
+    arguments = sys.argv[1:]
+    path = sys.argv[1]
+    print(path)
+    fo = open_file(path)
+
+    # Iterate over the characters in the file
+    while True:
+        char = get_next_character(fo)
+        if not char:
+            break
+        print(char, end='')
+
+    # Reset the file pointer to the beginning of the file
+    fo.seek(0)
+
+    # Iterate over the non-whitespace characters in the file
+    while True:
+        char = get_next_non_whitespace_character(fo)
+        if not char:
+            break
+        print(char, end='')
+
+    # Reset the file pointer to the beginning of the file
+    fo.seek(0)
+
+    # Iterate over the numbers in the file
+    while True:
+        number = get_next_number(fo)
+        if not number:
+            break
+        print(number[0], end=' ')  # print the integer representation of the number
+        if number[1] == '':
+            break
+
+    fo.seek(0)  # reset file pointer to beginning of file
+    name, char = get_next_name(fo)
+    while name is not None:
+        print(name)
+        name, char = get_next_name(fo)
+
+    if len(arguments) != 1:
+        print("Error! One command line argument is required.")
+        sys.exit()
+
+    else:
+
+        print("\nNow opening file...")
+        # Print the path provided and try to open the file for reading
+
+        print("\nNow reading file...")
+        # Print out all the characters in the file, until the end of file
+
+        print("\nNow skipping spaces...")
+        # Print out all the characters in the file, without spaces
+
+        print("\nNow reading numbers...")
+        # Print out all the numbers in the file
+
+        print("\nNow reading names...")
+        # Print out all the names in the file
+
+        print("\nNow censoring bad names...")
+        # Print out only the good names in the file
+        
+        # Reset the file pointer to the beginning of the file
+        fo.seek(0)
+        name, char = get_next_name(fo)
+
+        name_table = MyNames()
+        bad_name_ids = [name.lookup("Terrible"), name.lookup("Horrid"),
+                        name.lookup("Ghastly"), name.lookup("Awful")]
+        
+        while name is not None:
+            name_id = name_table.lookup(name)
+            if name_id not in bad_name_ids:
+                print(name_table.get_string(name_id))
+
+            name, char = get_next_name(fo)
+        # Close the file
+        fo.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ prelim/mynames.py
@@ -0,0 +1,49 @@
+"""Implements a name table for lexical analysis.
+
+Classes
+-------
+MyNames - implements a name table for lexical analysis.
+"""
+
+
+class MyNames:
+
+    """Implements a name table for lexical analysis.
+
+    Parameters
+    ----------
+    No parameters.
+
+    Public methods
+    -------------
+    lookup(self, name_string): Returns the corresponding name ID for the
+                 given name string. Adds the name if not already present.
+
+    get_string(self, name_id): Returns the corresponding name string for the
+                 given name ID. Returns None if the ID is not a valid index.
+    """
+
+    def __init__(self):
+        """Initialise the names list."""
+        self.names = []
+
+    def lookup(self, name_string):
+        """Return the corresponding name ID for the given name_string.
+
+        If the name string is not present in the names list, add it.
+        """
+        if name_string in self.names:
+            return self.names.index(name_string)
+        else:
+            self.names.append(name_string)
+            return len(self.names) - 1
+
+    def get_string(self, name_id):
+        """Return the corresponding name string for the given name_id.
+
+        If the name ID is not a valid index into the names list, return None.
+        """
+        if 0 <= name_id <= len(self.names) - 1:
+            return self.names[name_id]
+        else:
+            return None
