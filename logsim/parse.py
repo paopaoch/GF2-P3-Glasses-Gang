6e6e9c741a780670e8e0dcad8190b384818aae51
paopaoch <paopaochofficial@gmail.com>
2023-05-20 18:43:52 +0100

    add parser logic

--- logsim/parse.py
+++ logsim/parse.py
@@ -9,6 +9,11 @@ Classes
 Parser - parses the definition file and builds the logic network.
 """
 
+from names import Names
+from network import Network
+from devices import Devices
+from monitors import Monitors
+from scanner import Symbol, Scanner
 
 class Parser:
 
@@ -33,12 +38,94 @@ class Parser:
     parse_network(self): Parses the circuit definition file.
     """
 
-    def __init__(self, names, devices, network, monitors, scanner):
-        """Initialise constants."""
+    def __init__(self, names: Names, devices: Devices, 
+                network: Network, monitors: Monitors, 
+                scanner: Scanner):
+
+        self.names = names
+        self.devices = devices
+        self.network = network
+        self.monitors = monitors
+        self.scanner = scanner
+
+        self.phase = None  # Tells you which phase of parsing its in
+        self.current_symbol = None
+        self.previous_symbol = None
+
+
+    @staticmethod
+    def check_sentence(sentence: str):
+        """Primary check if there is a syntax error using regex
+        Returns False and the sentence type if the sentence does 
+        not have any syntax errors. True otherwise with None 
+        as sentence type."""
+        sentence_type = "init device"
+        return False, sentence_type
+
+
+    def classify_syntax_error(self, sentence: str):
+        """Check for the first syntax error in the sentence"""
+        pass
+        # 
+        # return self.error_type
+
+
+    def update_symbol(self):
+        self.previous_symbol = self.current_symbol
+        self.current_symbol = self.scanner.get_symbol()
+
+    def go_to_next_sentence(self):
+        while self.current_symbol != self.scanner.SEMICOLON:
+            self.update_symbol(self)
+
 
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
+
+        oututs = {}
+        inputs = {}
+        connections = {}
+
+
+        self.update_symbol(self)
+
+        if self.phase == None:
+            if self.current_symbol.type == self.scanner.INIT:
+                self.phase = 1
+            else:
+                raise SyntaxError("SYNTAX[Incomplete File]: Missing start mark") # Will have to change this
+        
+        self.update_symbol(self)
+        if self.current_symbol.type != self.scanner.SEMICOLON:
+            raise SyntaxError("SYNTAX[No Termination]: Missing termination mark")
+        
+        while self.phase == 1:
+            self.update_symbol(self)
+
+            # Deal with syntax error
+            if self.previous_symbol.type == self.scanner.SEMICOLON:
+                current_sentence = " ".join(self.scanner.get_sentence())
+                syntax_error, sentence_type = self.check_sentence(current_sentence)
+                if  not syntax_error:
+                    error = self.classify_syntax_error(current_sentence)
+                    self.go_to_next_sentence(self)
+                    continue
+                    raise SyntaxError(error)
+
+            # Everything into hashes. Hashes are great.
+            if sentence_type == "init device":
+                if self.current_symbol.type == self.scanner.DEVICE_NAME:
+                    if self.current_symbol not in oututs:
+                        oututs.add(self.current_symbol)
+
+                if self.current_symbol.type == self.scanner.DEVICE_TYPE:
+                    device = self.current_symbol
+
+                if self.current_symbol.type == self.scanner.NUMBER:
+                    if self.current_symbol > 3:
+                        pass
+
         return True
