bd8994833f84b635c3585cefdbd87ada19eb6337
paopaoch <paopaochofficial@gmail.com>
2023-05-31 14:52:33 +0100

    add a bunch of comments

--- logsim/error.py
+++ logsim/error.py
@@ -89,7 +89,7 @@ class Error:
         
         elif error_type == self.SEMANTIC:
             if self.error_code == self.devices.INVALID_QUALIFIER: # Checks for the number values
-                error_mes = f"SEMANTIC[INIT]: Invalid Device Qualifier {optional_mess}"
+                error_mes = f"SEMANTIC[INIT]: Device is initialised with wrong qualifier {optional_mess}"
             elif self.error_code == self.devices.NO_QUALIFIER: # Check for the whether the number exists
                 error_mes = f"SEMANTIC[INIT]: No quaifier inputed {optional_mess}"
             elif self.error_code == self.devices.BAD_DEVICE:
